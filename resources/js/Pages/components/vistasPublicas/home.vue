58e4362dbfe0b79acde5a447ffcd36e1c37ede23
Britany Sherlyn <115283288+BritanyOlguin@users.noreply.github.com>
2024-01-09 09:33:03 -0600

    EstructuraImgITI
    Para carrusel solo tenga Id Titulo Imagen

--- resources/js/Pages/components/vistasPublicas/home.vue
+++ resources/js/Pages/components/vistasPublicas/home.vue
@@ -1,5 +1,6 @@
 <script setup>
-import carruselEstructura from '../estructuras/carruselEstructura.vue';
+// import carruselEstructura from '../estructuras/carruselEstructura.vue';
+import EstructuraImgITI from '../estructuras/estructuraImgITI.vue';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 </script>
@@ -8,7 +9,7 @@ import TabPanel from 'primevue/tabpanel';
 <template>
     <TabView ref="tabview1" class="p-3">
         <TabPanel header="Carrusel Principal">
-            <carruselEstructura :Titulo="'Titulo'" :Subtitulo="'Link'" :loadDataUrl="'/home/bannerData'" :registerBannerUrl="'/home/registrarBanner'" :editBannerUrl="'/home/editarBanner'" :deleteBannerUrl="'/home/eliminarBanner'" />
+            <EstructuraImgITI :Titulo="'Titulo'" :Subtitulo="'Link'" :loadDataUrl="'/home/bannerData'" :registerBannerUrl="'/home/registrarBanner'" :editBannerUrl="'/home/editarBanner'" :deleteBannerUrl="'/home/eliminarBanner'" />
         </TabPanel>
     </TabView>
 </template>
