d6c236d96695adf807bf18a41b1a4e2ae740010f
Britany Sherlyn <115283288+BritanyOlguin@users.noreply.github.com>
2024-01-24 10:35:08 -0600

    Rutas para Admin arreglado

--- resources/js/Router/index.js
+++ resources/js/Router/index.js
@@ -1,36 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeAdmin from '../Pages/components/vistasAdmin/homeAdmin.vue';
 import ProductosAdmin from '../Pages/components/vistasAdmin/productosAdmin.vue';
 import NosotrosAdmin from '../Pages/components/vistasAdmin/nosotrosAdmin.vue';
 import AvisoPrivacidadAdmin from '../Pages/components/vistasAdmin/avisoPrivacidadAdmin.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: HomeAdmin,
-        },
-        {
-            path: '/productos',
-            name: 'productos',
-            component: ProductosAdmin,
-        },
-        {
-            path: '/nosotros',
-            name: 'nosotros',
-            component: NosotrosAdmin,
-        },
-        {
-            path: '/avisoPrivacidad',
-            name: 'avisoPrivacidad',
-            component: AvisoPrivacidadAdmin,
-        },
-        {
-            path: '/auth/login',
-            name: 'login',
-            component: () => import('../pages/auth/Login.vue')
+            children: [
+                {
+                    path: '/',
+                    name: 'home',
+                    component: HomeAdmin,
+                },
+                {
+                    path: '/productos',
+                    name: 'productos',
+                    component: ProductosAdmin,
+                },
+                {
+                    path: '/nosotros',
+                    name: 'nosotros',
+                    component: NosotrosAdmin,
+                },
+                {
+                    path: '/avisoPrivacidad',
+                    name: 'avisoPrivacidad',
+                    component: AvisoPrivacidadAdmin,
+                },
+                {
+                    path: '/auth/login',
+                    name: 'login',
+                    component: () => import('../pages/auth/Login.vue')
+                },
+            ],
         },
     ],
 });
