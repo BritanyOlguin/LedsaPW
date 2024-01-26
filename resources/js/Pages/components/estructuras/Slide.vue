220ddf6a1805c8f68acb2b21e03f42a1bee931bd
Britany Sherlyn <115283288+BritanyOlguin@users.noreply.github.com>
2024-01-26 12:02:38 -0600

    Transicion
    Identifica cuando ya se haya terminado e iniciado una transicion

--- resources/js/Pages/components/estructuras/Slide.vue
+++ resources/js/Pages/components/estructuras/Slide.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="slide">
-        <Transition name="slide">
+        <Transition name="slide" @after-enter="onEnter" @after-leave="onLeave">
             <slot />
         </Transition>
     </div>
@@ -8,12 +8,18 @@
 
 <script>
 export default {
-
+    methods: {
+        onEnter() {
+            this.$emit('transitionEnd', true);  // Emitir un evento al finalizar la transición
+        },
+        onLeave() {
+            this.$emit('transitionEnd', false);  // Emitir un evento al iniciar la transición
+        }
+    }
 }
 </script>
 
 <style lang="scss">
-
 .slide-enter-active,
 .slide-leave-active {
     transition: opacity 0.7s ease-in-out;
@@ -28,5 +34,4 @@ export default {
 .slide-leave-from {
     opacity: 1;
 }
-
 </style>
