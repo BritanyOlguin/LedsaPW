db4f870f6f4fbf655565442ea9c3b1463f13082f
Britany Sherlyn <115283288+BritanyOlguin@users.noreply.github.com>
2024-02-14 12:18:38 -0600

    Prueba

--- resources/js/Pages/components/vistasPublicas/listaContactos.vue
+++ resources/js/Pages/components/vistasPublicas/listaContactos.vue
@@ -14,6 +14,33 @@ export default {
                 console.log(error);
             });
         },
+        async copiarAlPortapapeles(texto) {
+            try {
+                await navigator.clipboard.writeText(texto);
+                alert("Número copiado correctamente!"); // Mejor usa una solución más elegante que alert
+            } catch (err) {
+                console.error("Error al copiar el texto: ", err);
+            }
+        },
+        // Método para manejar el mantener presionado
+        manejarMantenerPresionado(contacto) {
+            const tiempoPresionado = 500; // Tiempo en ms para considerar como mantener presionado
+            let timerId;
+
+            const iniciarTemporizador = () => {
+                timerId = setTimeout(() => this.copiarAlPortapapeles(contacto.contenido), tiempoPresionado);
+            };
+
+            const cancelarTemporizador = () => {
+                clearTimeout(timerId);
+            };
+
+            return {
+                onTouchStart: iniciarTemporizador,
+                onTouchEnd: cancelarTemporizador,
+                onTouchMove: cancelarTemporizador, // Cancela si el usuario mueve el dedo
+            };
+        },
     },
     computed: {
         contactosPorDepartamento() {
@@ -41,13 +68,13 @@ export default {
     <div class="departamentos-container">
         <div class="departamento" v-for="(grupo, departamento) in contactosPorDepartamento" :key="departamento">
             <h3 style="font-size: 1.9em; font-weight: bold;">{{ departamento }}</h3>
-            <div v-if="grupo.length > 0" class="contacto-info">
+            <div v-if="grupo.length > 0" class="contacto-info" v-bind="manejarMantenerPresionado(contacto)">
                 <i class="pi pi-envelope icono"></i>
-                <p style="font-size: 1.2em; font-weight: bold;" >{{ grupo[0].departamento.link }}</p>
+                <p style="font-size: 1.2em; font-weight: bold;">{{ grupo[0].departamento.link }}</p>
             </div>
             <div v-for="contacto in grupo" :key="contacto.id" class="contacto">
                 <p class="titulo">{{ contacto.titulo }}</p>
-                <div class="contacto-info">
+                <div class="contacto-info" v-bind="manejarMantenerPresionado(contacto)">
                     <i class="pi pi-phone icono"></i>
                     <p>{{ contacto.contenido }}</p>
                 </div>
